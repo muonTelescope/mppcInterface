3182100d8a05504d81eb778188567ba2160225a9
sawaiz <github@sawaizsyed.com>
2016-05-18 20:30:41 -0400

    Removed code directory

--- code/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/oneDrive/projects/mppcInterface/
-tar -czf ~/code.tar.gz code
-scp ~/code.tar.gz $1:.
-scp ~/oneDrive/projects/mppcInterface/code/remoteUpload.sh $1:.
-scp ~/oneDrive/projects/mppcInterface/code/setup.sh $1:.
-rm ~/code.tar.gz
